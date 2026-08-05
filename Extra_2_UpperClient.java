efe8c7acbf8f1819833e2865018c40d205b0b5ff
yuva-123456 <yashu.spam2006@gmail.com>
2026-08-05 23:07:27 +0530

    Create Extra_2_UpperClient.java

---
+++ Extra_2_UpperClient.java
@@ -0,0 +1,39 @@
+import java.io.*;
+import java.net.*;
+import java.util.Scanner;
+
+public class UpperClient {
+
+    public static void main(String[] args) throws Exception {
+
+        Socket socket = new Socket("localhost", 5000);
+
+        BufferedReader in = new BufferedReader(
+                new InputStreamReader(socket.getInputStream()));
+
+        PrintWriter out = new PrintWriter(
+                socket.getOutputStream(), true);
+
+        Scanner sc = new Scanner(System.in);
+
+        String message;
+
+        while (true) {
+
+            System.out.print("Enter Message: ");
+            message = sc.nextLine();
+
+            out.println(message);
+
+            String reply = in.readLine();
+
+            System.out.println("Server: " + reply);
+
+            if (message.equalsIgnoreCase("bye"))
+                break;
+        }
+
+        socket.close();
+        sc.close();
+    }
+}
